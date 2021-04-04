43379ebbd132c108651abc463312dccdb3397225
Caio Jesus <caioj3505@gmail.com>
2021-04-03 22:45:35 -0300

    adcionando consumer

--- lib/components/task_item.dart
+++ lib/components/task_item.dart
@@ -6,63 +6,65 @@ import 'package:taskee/models/task_model.dart';
 class TaskItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final TaskModel task = Provider.of<TaskModel>(context);
-    return GestureDetector(
-      onTap: () {
-        task.toggleCheck();
-      },
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-        child: Dismissible(
-          onDismissed: null,
-          direction: DismissDirection.startToEnd,
-          key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-          background: Container(
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Align(
-              alignment: Alignment(-0.9, 0.0),
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
+    final TaskModel task = Provider.of<TaskModel>(context, listen: false);
+    return Consumer<TaskModel>(
+      builder: (ctx, task, __) => GestureDetector(
+        onTap: () {
+          task.toggleCheck();
+        },
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+          child: Dismissible(
+            onDismissed: null,
+            direction: DismissDirection.startToEnd,
+            key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+            background: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Align(
+                alignment: Alignment(-0.9, 0.0),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          child: OutlineGradientButton(
-            backgroundColor: Colors.transparent,
-            radius: Radius.circular(0),
-            strokeWidth: 2,
-            padding: EdgeInsets.only(top: 10, bottom: 10),
-            gradient: LinearGradient(
-              colors: task.isChecked == true
-                  ? [
-                      Theme.of(context).primaryColor,
-                      Theme.of(context).primaryColorLight,
-                    ]
-                  : [
-                      Theme.of(context).disabledColor,
-                      Theme.of(context).disabledColor,
-                    ],
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-            ),
-            child: ListTile(
-              title: Text(
-                task.title,
-                style: Theme.of(context).textTheme.headline2,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 2,
-                softWrap: true,
+            child: OutlineGradientButton(
+              backgroundColor: Colors.transparent,
+              radius: Radius.circular(0),
+              strokeWidth: 2,
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              gradient: LinearGradient(
+                colors: task.isChecked == true
+                    ? [
+                        Theme.of(context).primaryColor,
+                        Theme.of(context).primaryColorLight,
+                      ]
+                    : [
+                        Theme.of(context).disabledColor,
+                        Theme.of(context).disabledColor,
+                      ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
               ),
-              trailing: Icon(
-                task.isChecked == true
-                    ? Icons.check_outlined
-                    : Icons.panorama_fish_eye,
-                color: task.isChecked == true
-                    ? Theme.of(context).primaryColorLight
-                    : Theme.of(context).disabledColor,
+              child: ListTile(
+                title: Text(
+                  task.title,
+                  style: Theme.of(context).textTheme.headline2,
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                  softWrap: true,
+                ),
+                trailing: Icon(
+                  task.isChecked == true
+                      ? Icons.check_outlined
+                      : Icons.panorama_fish_eye,
+                  color: task.isChecked == true
+                      ? Theme.of(context).primaryColorLight
+                      : Theme.of(context).disabledColor,
+                ),
               ),
             ),
           ),
