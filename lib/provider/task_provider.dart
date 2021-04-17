8f8820946f0c104277650ee54aac1edac606cb8b
Caio Jesus <caioj3505@gmail.com>
2021-04-17 18:13:57 -0300

    Implementa método para carregar itens do firebase

--- lib/provider/task_provider.dart
+++ lib/provider/task_provider.dart
@@ -1,33 +1,37 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
-import 'package:taskee/data/dummy_data.dart';
 import 'package:taskee/provider/task_model.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:taskee/utils/api_key.dart';
 
 class TaskProvider with ChangeNotifier {
-  List<TaskModel> _items = DUMMY_DATA;
+  List<TaskModel> _items = [];
   List<TaskModel> get items => [..._items];
 
+  Future<void> loadItems() async {
+    http.Response response = await http.get(Uri.parse(url));
+    Map<String, dynamic> data = json.decode(response.body);
+    data.forEach((id, data) {
+      _items.add(TaskModel(
+        id: id,
+        title: data['title'],
+        isChecked: data['isChecked'],
+      ));
+    });
+    notifyListeners();
+  }
+
   void addItem(TaskModel task) async {
-    const url =
-        'https://taskee-app-f5e80-default-rtdb.firebaseio.com/tasks.json';
-
-    try {
-      await http.post(
-        Uri.parse(url),
-        body: json.encode({
-          'id': task.id,
-          'title': task.title,
-          'isChecked': task.isChecked,
-        }),
-      );
-
-      _items.add(task);
-      notifyListeners();
-    } catch (error) {
-      throw Exception(error);
-    }
+    await http.post(
+      Uri.parse(url),
+      body: json.encode({
+        'id': task.id,
+        'title': task.title,
+        'isChecked': task.isChecked,
+      }),
+    );
+    _items.add(task);
+    notifyListeners();
   }
 
   void remove(String id) {
--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:taskee/provider/task_model.dart';
 import 'package:taskee/widgets/task_item.dart';
 import 'package:taskee/provider/task_provider.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  @override
+  void initState() {
+    super.initState();
+    Provider.of<TaskProvider>(context, listen: false).loadItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     final task = Provider.of<TaskProvider>(context, listen: true);
     final taskList = task.items;
     return Padding(
-      padding: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.only(top: 5),
       child: ListView.builder(
         itemCount: taskList.length,
         itemBuilder: (context, i) => ChangeNotifierProvider.value(
