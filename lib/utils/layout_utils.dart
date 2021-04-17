d05c1582bf6b226d95b53d209598b0a6399c3a54
Caio Jesus <caioj3505@gmail.com>
2021-04-16 23:15:49 -0300

    Implementa responsividade dos widgets

---
+++ lib/utils/layout_utils.dart
@@ -0,0 +1,10 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class LayoutUtils {
+  BuildContext context;
+  LayoutUtils(this.context);
+  bool isLargeScreen() => MediaQuery.of(context).size.width > 600;
+  double resizeWidget(double valor) => isLargeScreen() ? valor * 1.6 : valor;
+}
--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:taskee/components/task_item.dart';
+import 'package:taskee/widgets/task_item.dart';
 import 'package:taskee/provider/task_provider.dart';
 
 class TaskList extends StatelessWidget {
@@ -8,11 +8,14 @@ class TaskList extends StatelessWidget {
   Widget build(BuildContext context) {
     final task = Provider.of<TaskProvider>(context, listen: true);
     final taskList = task.items;
-    return ListView.builder(
-      itemCount: taskList.length,
-      itemBuilder: (context, i) => ChangeNotifierProvider.value(
-        value: taskList[i],
-        child: TaskItem(),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: ListView.builder(
+        itemCount: taskList.length,
+        itemBuilder: (context, i) => ChangeNotifierProvider.value(
+          value: taskList[i],
+          child: TaskItem(),
+        ),
       ),
     );
   }
